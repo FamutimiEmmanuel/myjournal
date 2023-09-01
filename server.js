41e5569031c1b03220dc02de07a7e34ecdff4a19
FamutimiEmmanuel <olamide.famutimi@gmail.com>
2023-09-01 19:19:15 +0100

    build

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(require('./routes/auth'))
 app.use(require('./routes/writers'))
 
 
-// if(process.env.NODE_ENV === 'production') {
-//     app.use(express.static('frontend/build'));
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
 
-//     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
-// }
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
